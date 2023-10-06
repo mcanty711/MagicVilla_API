f00c4140af3dcfae0cd52df56be726ae5132e239
Marcus Canty <marcusjcanty@gmail.com>
2023-10-06 14:23:23 -0400

    oct 6 14:23

--- MagicVilla_VillaAPI/Models/VillaNumber.cs
+++ MagicVilla_VillaAPI/Models/VillaNumber.cs
@@ -12,6 +12,7 @@ namespace MagicVilla_VillaAPI.Models
         public int VillaID { get; set; }
 
         public Villa Villa { get; set; }
+
         public string SpecialDetails { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime UpdatedDate { get; set; }
